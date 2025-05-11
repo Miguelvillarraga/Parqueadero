64a5d940157b0f8d572f59c6475b6df6d2f9803d
Miguel Villarraga Franco <143026348+Miguelvillarraga@users.noreply.github.com>
2025-05-11 14:42:02 -0500

    Update setup.py

--- Database/setup.py
+++ Database/setup.py
@@ -1,27 +1,28 @@
-import sqlite3
-
-conn = sqlite3.connect("parqueadero.db")
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Vehiculo (
-    Placa TEXT PRIMARY KEY,
-    Tipo TEXT NOT NULL,
-    Usuario TEXT NOT NULL
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Registro (
-    ID_Registro INTEGER PRIMARY KEY AUTOINCREMENT,
-    Placa_Vehiculo TEXT NOT NULL,
-    Fecha_Entrada DATE NOT NULL,
-    Hora_Entrada TIME NOT NULL,
-    Fecha_Salida DATE,
-    Hora_Salida TIME,
-    FOREIGN KEY (Placa_Vehiculo) REFERENCES Vehiculo(Placa)
-)
-""")
-
-conn.commit()
-conn.close()
+def crear_tablas():
+    import sqlite3
+    
+    conn = sqlite3.connect("parqueadero.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Vehiculo (
+        Placa TEXT PRIMARY KEY,
+        Tipo TEXT NOT NULL,
+        Usuario TEXT NOT NULL
+    )
+    """)
+    
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Registro (
+        ID_Registro INTEGER PRIMARY KEY AUTOINCREMENT,
+        Placa_Vehiculo TEXT NOT NULL,
+        Fecha_Entrada DATE NOT NULL,
+        Hora_Entrada TIME NOT NULL,
+        Fecha_Salida DATE,
+        Hora_Salida TIME,
+        FOREIGN KEY (Placa_Vehiculo) REFERENCES Vehiculo(Placa)
+    )
+    """)
+    
+    conn.commit()
+    conn.close()
