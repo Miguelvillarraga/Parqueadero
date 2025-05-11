ea7a0cf16c103c89dc6e39fd87ff89bf9ef9ec5c
Miguel Villarraga Franco <143026348+Miguelvillarraga@users.noreply.github.com>
2025-05-11 14:57:49 -0500

    Update Registro.py

--- Database/Models/Registro.py
+++ Database/Models/Registro.py
@@ -8,7 +8,7 @@ class Registro:
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute("""
-            SELECT Vehiculo.Placa, Vehiculo.Tipo, Vehiculo.Usuario, Registro.HoraEntrada, Registro.HoraSalida
+            SELECT Vehiculo.Placa, Vehiculo.Tipo, Vehiculo.Usuario, Registro.Fecha_Entrada, Registro.Hora_Entrada, Registro.Fecha_Salida,  Registro.Hora_Salida
             FROM Registro
             JOIN Vehiculo ON Registro.Placa = Vehiculo.Placa
             ORDER BY Registro.HoraEntrada DESC
@@ -20,8 +20,10 @@ class Registro:
                 'placa': row[0],
                 'tipo': row[1],
                 'usuario': row[2],
-                'hora_entrada': row[3],
-                'hora_salida': row[4]
+                'fecha_entrada': row[3],
+                'hora_entrada': row[4],
+                'fecha_salida': row[5],
+                'hora_salida': row[6]
             }
             for row in rows
         ]
